3c92fe56c6d706785a528b5873e6f072af97d0a1
perak <petar.korponaic@gmail.com>
2014-04-23 13:04:34 +0200

    About Views and example

--- client/views/examples/example5/example5.js
+++ client/views/examples/example5/example5.js
@@ -1,3 +1,38 @@
+Template.ExamplesExample5.rendered = function() {
+	require("famous-polyfills"); // Add polyfills
+	require("famous/core/famous"); // Add the default css file
+
+	var Engine     = require("famous/core/Engine");
+	var Surface    = require("famous/core/Surface");
+	var Scrollview = require("famous/views/Scrollview");
+
+	var destNode = document.getElementById("example-container") || undefined;
+	var context = Engine.createContext(destNode);
+
+	var scrollview = new Scrollview();
+
+	var surfaces = [];
+
+	scrollview.sequenceFrom(surfaces);
+
+	for (var i = 0; i < 100; i++) {
+		var temp = new Surface({
+			content: "Surface: " + (i + 1),
+			size: [undefined, 100],
+			properties: {
+				backgroundColor: "hsl(" + (i * 360 / 40) + ", 100%, 50%)",
+				lineHeight: "100px",
+				textAlign: "center"
+			}
+		});
+
+		temp.pipe(scrollview);
+		surfaces.push(temp);
+	}
+
+	context.add(scrollview);
+}
+
 Template.ExamplesExample5.events({
 
 });
